fa5b25eca6cb900a3caa4c7e56735c427c9dbd35
lancejosephmdayawon <lancejosephmdayawon@gmail.com>
2025-10-30 19:39:42 +0800

    deleted duplicated files

--- basic_function.py
+++
@@ -1,9 +0,0 @@
-def add():
-    x=9
-    y=10
-    z=x+y
-    return z
-
-zeb=add()
-
-print(zeb)
--- basic_if_elseif.py
+++
@@ -1,6 +0,0 @@
-number = int(input("Enter a number: "))
-
-if number > 0:
-    print (f"{number} is a positive number")
-
-print ("A statement outside if-statement.")
